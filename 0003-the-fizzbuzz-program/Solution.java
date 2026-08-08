b0f7a698c49ee2107d45f62b37db6184c228bf7f
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-08 12:53:21 +0530

    Add The FizzBuzz Program solution

---
+++ 0003-the-fizzbuzz-program/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if ( n % 15 == 0)System.out.println("FizzBuzz");
+        else if ( n % 3 == 0)System.out.println("Fizz");
+        else if ( n % 5 == 0)System.out.println("Buzz");
+        // code here
+        else System.out.println(n);
+        
+    }
+}
