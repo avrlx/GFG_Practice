7bebf3d036c5f11bb970ec6a39d6d79238c20842
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-15 12:42:46 +0530

    Add Search in Rotated Sorted Array solution

---
+++ 0010-search-in-a-rotated-array4618/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    int search(int[] arr, int key) {
+        // code here
+        int low =0 ;
+        int high= arr.length - 1;
+        while (low <= high){
+            int mid = low + (high - low )/2;
+            if(arr[mid] == key) return mid;
+            else if(arr[low] < arr[mid]){
+                if(arr[low] < key && arr[mid] > key) high = mid -1;
+                else low = mid +1;
+            }
+            else{
+                if(arr[high] > key && arr[mid] < key) low = mid +1;
+                else high = mid -1;
+            }
+        }
+        return -1;
+    }
+}
