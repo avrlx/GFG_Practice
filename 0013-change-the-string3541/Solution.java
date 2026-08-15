a2bb350e3c05125e4fc470a6f8cd5109aa4ea52d
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-15 22:59:09 +0530

    Add Change the Case based on First Character solution

---
+++ 0013-change-the-string3541/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    String modify(String s) {
+        if(s.charAt(0) >= 'a' && s.charAt(0) <= 'z') 
+        return s.toLowerCase();
+        return s.toUpperCase();
+        
+    }
+}
