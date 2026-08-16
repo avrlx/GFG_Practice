2c07b4fedc4d0a2edfa752497424e76c3de20d67
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-16 11:38:07 +0530

    Add Sum of all substrings of a number solution

---
+++ 0014-sum-of-all-substrings-of-a-number/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int sumSubstrings(String s) {
+        // code here
+        int sum = 0;
+        for(int i = 0;i<s.length();i++){
+            for(int j = i;j<s.length();j++){
+                String str = s.substring(i,j+1);
+                int n = Integer.valueOf(str);
+                sum += n;
+            }
+        }
+        return sum;
+    }
+}
